082bcac50ea917c12459998af28c2636b23b57f6
Mustafa Mirac İnce <94219972+MiracleInce@users.noreply.github.com>
2022-10-22 22:50:49 +0300

    Add files via upload

---
+++ KDV.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class KDV {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        double tutar, KDVmiktar,sonuc;
+        System.out.println("Lütfen paranızın tutarını giriniz");
+        tutar =input.nextDouble();
+        boolean kdv1= tutar>=1000 ;
+        boolean kdv2= tutar<1000;
+        double kdv = (kdv1) ? 0.18 : 0.8 ;
+        KDVmiktar = kdv*tutar;
+        sonuc=tutar+KDVmiktar;
+        System.out.println("KDV\'siz tutar: "+tutar);
+        System.out.println("KDV oranı: "+kdv);
+        System.out.println("KDV miktarı: "+KDVmiktar);
+        System.out.println("KDV\'li tutar: "+sonuc);
+
+
+
+
+
+
+
+
+    }
+}
