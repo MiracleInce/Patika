6ca9c80894bab2454574c4f12bed46dd1eeaccb4
Mustafa Mirac İnce <94219972+mustafamiracince@users.noreply.github.com>
2022-12-06 16:07:28 +0300

    Add files via upload

---
+++ Maas Hesaplayici/src/Employee.java
@@ -0,0 +1,58 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Employee {
+    String name;
+    int salary;
+    int workHours;
+    int hireYear;
+    Employee(String name, int salary, int workHours, int hireYear){
+        this.name=name;
+        this.salary=salary;
+        this.workHours=workHours;
+        this.hireYear=hireYear;
+    }
+    double tax(){
+        double tax=0.0;
+        if (this.salary<1000){
+            System.out.println("Vergi uygulanmadı");
+        }else{
+            tax=this.salary*0.03;
+        }
+        return tax;
+    }
+    int bonus(){
+        int bonus=0;
+        if (this.workHours>40){
+            bonus=(this.workHours-40)*30;
+        }
+        return bonus;
+    }
+    double raiseSalary(){
+        int sure= 2021-this.hireYear;
+        double artis=0.0;
+        if(sure<10){
+            artis=this.salary*0.05;
+            return artis;
+        } else if (sure>9 && sure<20 ) {
+            artis=(this.salary*0.1);
+            return artis;
+        } else if (sure>19) {
+            artis=(this.salary*0.15);
+            return artis;
+        }
+        return artis;
+    }
+    void info(){
+        double vergiBonus=this.salary+bonus()-tax();
+        double toplamMaas=this.salary+raiseSalary();
+        System.out.println("Adı: "+ this.name);
+        System.out.println("Maaşı: "+ this.salary);
+        System.out.println("Çalışma Saati: "+ this.workHours);
+        System.out.println("Başlangıç Yılı: "+ this.hireYear);
+        System.out.println("Vergi: "+ this.tax());
+        System.out.println("Bonus: "+ this.bonus());
+        System.out.println("Maaş Artışı: "+ this.raiseSalary());
+        System.out.println("Vergi ve Bonuslar ile birlikte maaş: "+vergiBonus );
+        System.out.println("Toplam Maaş: " +toplamMaas);
+    }
+}
