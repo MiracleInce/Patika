bec10d3f80200df60fb6c638c7f867811aa4b444
Mustafa Mirac İnce <94219972+mustafamiracince@users.noreply.github.com>
2022-12-06 12:40:01 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,36 +1,18 @@
-import com.sun.security.jgss.GSSUtil;
-
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        double n1, n2;
-        int islem;
-        System.out.println("Lütfen işlem yapmak istediğiniz ilk sayıyı giriniz");
-        n1 = input.nextDouble();
-        System.out.println("Lütfen işlem yapmak istediğiniz ikinci sayıyı giriniz");
-        n2 = input.nextDouble();
-        System.out.println("Yapmak istediğiniz işlemi seçiniz\n 1-Toplama \t 2-Çıkarma \t 3-Çarpma \t 4-Bölme");
-        islem = input.nextInt();
-        switch (islem) {
-            case 1:
-                System.out.println(n1 + n2);
-                break;
-            case 2 :
-                System.out.println(n1 - n2);
-                break;
-            case 3:
-                System.out.println(n1 * n2);
-                break;
-            case 4:
-                if (n2 != 0)
-                    System.out.println(n1 / n2);
-                else
-                    System.out.println("Bir sayı 0'a bölünemez");
-                break;
-            default:
-                System.out.println("Lütfen geçerli bir işlem giriniz");
-                break;
-        }
+        Teacher t1=new Teacher("Abdullah", "555", "Tarih");
+        Teacher t2=new Teacher("Hüseyin", "553", "Fizik");
+        Teacher t3=new Teacher("Yaşar", "505","Biyoloji");
+        Course fizik=new Course("Fizik", "FZK", "Fizik");
+        Course biyoloji=new Course("Biyoloji", "BİYO", "Biyoloji");
+        Course tarih=new Course("Tarih", "TRH" ,"Tarih");
+        tarih.addTeacher(t1);
+        biyoloji.addTeacher(t3);
+        fizik.addTeacher(t2);
+        Student  s1=new Student("Mirac", "132", "2", tarih, biyoloji , fizik);
+        s1.notAtama(60,75,83,80,70,90);
+        s1.printNote();
+        s1.isPass();
+
     }
 }
