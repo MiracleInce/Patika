1c8b3e74dc70a90e2df7e4852d008106862756d8
Mustafa Mirac İnce <94219972+mustafamiracince@users.noreply.github.com>
2022-12-09 18:45:37 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        int[] harmonik = {4,6};
+        double series=0.0;
+        for (double arr : harmonik) {
+            series+=(1/arr);
+        }
+        double avarege=harmonik.length/series;
+        System.out.println(avarege);
+    }
+}
